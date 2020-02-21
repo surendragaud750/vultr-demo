f0eaddab63708768082fcc57a6283646a6709025
Surendra Gaud <surendragaud750@gmail.com>
2020-02-21 19:05:32 +0530

    Added JS files for dashboard theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,57 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+// Core JS Files   -->
+//= require perfect-scrollbar.jquery.min
+
+// Plugin for the momentJs  -->
+//= require moment.min
+
+// Plugin for Sweet Alert -->
+//= require sweetalert2
+
+// Forms Validations Plugin -->
+//= require jquery.validate.min
+
+// Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+//= require jquery.bootstrap-wizard
+
+// Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+//= require bootstrap-selectpicker
+
+// Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+//= require bootstrap-datetimepicker.min
+
+// DataTables.net Plugin, full documentation here: https://datatables.net/  -->
+//= require jquery.dataTables.min
+
+// Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+//= require bootstrap-tagsinput
+
+// Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+//= require jasny-bootstrap.min
+
+// Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+//= require fullcalendar.min
+
+// Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+//= require jquery-jvectormap
+
+// Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+//= require nouislider.min
+
+// Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
+// Library for adding dinamically elements -->
+//= require arrive.min
+
+// Chartist JS -->
+//= require chartist.min
+
+//  Notifications Plugin    -->
+//= require bootstrap-notify
+
+// Material Dashboard DEMO methods, don't include it in your project! -->
+//= require demo
+
 //= require activestorage
 //= require turbolinks

a203695eff0f472ab78cd3b7ea933c9f881cfb48
Surendra Gaud <surendragaud750@gmail.com>
2020-02-22 20:13:10 +0530

    Added email for support

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class DashboardController < ApplicationController
   def index;end
+
   def products;end
+
   def create_product
+  	@product = params[:products]
+  	ProductMailer.send_new_product_to_support(@product).deliver_now
+  	redirect_to root_path, notice: "Product Cretaed kjasbckjn"
   end
 end

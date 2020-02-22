8767dedb001bb8db5de92bfd8de3f601c5f1233e
Surendra Gaud <surendragaud750@gmail.com>
2020-02-23 00:10:25 +0530

    Change email

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -3,10 +3,11 @@ class ProductMailer < ApplicationMailer
 	def send_new_product_to_support(product)
 		@product = product
 		mail(
-		  to: "support@technowand.com.au",
+		  # to: "support@technowand.com.au",
+		  to: "kanakshrma52@gmail.com",
       subject: "New product is created.",
       from: "admin@vultrdemo.com"
     )
   end
-  
+
 end
